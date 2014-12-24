72d9ed3ddc75af019b318ae22827465e566b02d7
Mike Singleton <mike@foursquare.com>
2014-12-24 02:28:02 +0000

    Init commit

---
+++ AutoLayoutTodayExtension/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutTodayExtension
+//
+//  Created by Mike Singleton on 12/23/14.
+//  Copyright (c) 2014 Mike Singleton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
